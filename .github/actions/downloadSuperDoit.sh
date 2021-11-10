51ef6187d3831616b5c88893b074d31722ea7e76
Dale Henrichs <dale.henrichs@gemtalksystems.com>
2021-11-10 10:38:30 -0800

    working on downloadSuperDoitProject step

--- .github/actions/downloadSuperDoit.sh
+++
@@ -1,6 +0,0 @@
-#! /usr/bin/env bash
-
-echo "superDoit_source = $superDoit_source"
-echo "superDoit_branch = $superDoit_branch"
-export superDoit_gemstone_version=3.6.1
-echo "::set-output name=gemstone-version::$superDoit_gemstone_version"
---
+++ .github/actions/downloadSuperDoitProject.sh
@@ -0,0 +1,4 @@
+#! /usr/bin/env bash
+
+echo "pwd=`pwd`"
+echo "HOME=$HOME"
