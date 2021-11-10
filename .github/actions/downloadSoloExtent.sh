af1e49040e9557d256be11850a5207220aecf31f
Dale Henrichs <dale.henrichs@gemtalksystems.com>
2021-11-10 13:04:36 -0800

    working on downloadSuperDoitSoloExtent step

--- .github/actions/downloadSoloExtent.sh
+++ .github/actions/downloadSoloExtent.sh
@@ -12,7 +12,7 @@ echo "Downloading extent0.solo.dbf for $soloExtentVersion ..."
 
 curl -s -S -L https://github.com/dalehenrich/superDoit/releases/download/v0.1.0/${soloExtentVersion}_extent0.solo.dbf.gz | gunzip -c > $superDoit_root_dir/gemstone/solo/extent0.solo.dbf
 
-chmod 0o444  $superDoit_root_dir/gemstone/solo/extent0.solo.dbf
+chmod 444 $superDoit_root_dir/gemstone/solo/extent0.solo.dbf
 
 echo "::set-output name=solo-extent-version::$soloExtentVersion"
 
