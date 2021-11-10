ddb193e5d348869f636a5e3121c58d630ef04750
Dale Henrichs <dale.henrichs@gemtalksystems.com>
2021-11-10 10:53:35 -0800

    working on downloadSuperDoitProject step

--- .github/actions/downloadSuperDoitProject.sh
+++ .github/actions/downloadSuperDoitProject.sh
@@ -1,6 +1,9 @@
 #! /usr/bin/env bash
 
-echo "hi there"
-pwd=`pwd`
-echo "$pwd"
-echo "HOME=$HOME"
+installation_directory="$HOME/.superDoit"
+
+if [ -d "$superDoit_source" ] ; then
+	"directory exists so no need to download the project"
+	exit 0
+fi
+echo "NEED TO DOWANLOAD ${superDoit_source}@${superDoit_source}"
