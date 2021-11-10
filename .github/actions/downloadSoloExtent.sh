498bc5d0439d5388e2fb6bfa79e39df4fcef1196
Dale Henrichs <dale.henrichs@gemtalksystems.com>
2021-11-10 13:03:16 -0800

    working on downloadSuperDoitSoloExtent step

--- .github/actions/downloadSoloExtent.sh
+++ .github/actions/downloadSoloExtent.sh
@@ -3,9 +3,9 @@
 set -e
 set -x
 
-soloExtentVersion = $superDoit_gemstone_version
-if [[ $extenVersion != '3.6.1' && $extenVersion != '3.6.0' ]] ; then
-	soloExtentVersion='3.6.1.
+soloExtentVersion=$superDoit_gemstone_version
+if [[ $extentVersion != '3.6.1' && $extentVersion != '3.6.0' ]] ; then
+	soloExtentVersion='3.6.1'
 fi
 
 echo "Downloading extent0.solo.dbf for $soloExtentVersion ..."
@@ -13,3 +13,6 @@ echo "Downloading extent0.solo.dbf for $soloExtentVersion ..."
 curl -s -S -L https://github.com/dalehenrich/superDoit/releases/download/v0.1.0/${soloExtentVersion}_extent0.solo.dbf.gz | gunzip -c > $superDoit_root_dir/gemstone/solo/extent0.solo.dbf
 
 chmod 0o444  $superDoit_root_dir/gemstone/solo/extent0.solo.dbf
+
+echo "::set-output name=solo-extent-version::$soloExtentVersion"
+
