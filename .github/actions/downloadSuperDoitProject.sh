5b926805529d9a6b5e20b92dfc922bbc9b3df305
Dale Henrichs <dale.henrichs@gemtalksystems.com>
2021-11-10 11:22:19 -0800

    working on downloadSuperDoitProject step

--- .github/actions/downloadSuperDoitProject.sh
+++ .github/actions/downloadSuperDoitProject.sh
@@ -5,9 +5,9 @@ installation_directory="$HOME/.superDoit"
 if [ -d "$superDoit_source" ] ; then
 	"directory exists so no need to download the project"
 	echo "using existing superDoit directory $superDoit_source"
-	echo "::set-output name=superDoit-installation-directory::$superDoit_source"
+	echo "::set-output name=installation-directory::$superDoit_source"
 	exit 0
 fi
 echo "Downloading ${superDoit_source}@${superDoit_branch} to ${installation_directory}/superDoit ..."
-echo "::set-output name=superDoit-installation-directory::$installation_directory/superDoit"
+echo "::set-output name=installation-directory::$installation_directory/superDoit"
 
