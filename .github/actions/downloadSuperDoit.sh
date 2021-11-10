220e0001938daf6e4ea97c38f9806fd0cf0ff616
Dale Henrichs <dale.henrichs@gemtalksystems.com>
2021-11-10 09:36:03 -0800

    input/ouput handling in bash?

--- .github/actions/downloadSuperDoit.sh
+++ .github/actions/downloadSuperDoit.sh
@@ -1,6 +1,5 @@
 #! /usr/bin/env bash
 
-
 echo "BRANCH: $superDoit_branch <=="
-export superDoit_gemstone_version=3.6.1
-echo "superDoit-gemstone-version: $superDoit_gemstone_version <=="
+superDoit_gemstone_version=3.6.1
+echo $superDoit_gemstone_version
