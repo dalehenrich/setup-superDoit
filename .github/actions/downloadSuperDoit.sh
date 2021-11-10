8d5b897c0b46a03783bd28f0d3df0756a3d7ca36
Dale Henrichs <dale.henrichs@gemtalksystems.com>
2021-11-10 09:57:15 -0800

    input/ouput handling in bash?

--- .github/actions/downloadSuperDoit.sh
+++ .github/actions/downloadSuperDoit.sh
@@ -1,4 +1,6 @@
 #! /usr/bin/env bash
 
+echo "superDoit_source = $superDoit_source"
+echo "superDoit_branch = $superDoit_branch"
 superDoit_gemstone_version=3.6.1
 echo "::set-output name=gemstone-version::$superDoit_gemstone_version"
