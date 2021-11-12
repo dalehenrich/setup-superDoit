243e3146c79a9fe802d39d808f389da50b129082
Dale Henrichs <dale.henrichs@gemtalksystems.com>
2021-11-12 11:14:31 -0800

    rename OUTPUT solo-extent-version to solo-version

--- .github/actions/downloadGemStoneProductTree.sh
+++ .github/actions/downloadGemStoneProductTree.sh
@@ -16,13 +16,13 @@ ${superDoit_root_dir}/dev/downloadGemStone.sh $superDoit_gemstone_version
 productName=GemStone64Bit${superDoit_gemstone_version}-${productExtension}
 echo "::set-output name=gemstone-product-name::$productName"
 echo "::set-output name=gemstone-product-path::${superDoit_root_dir}/gemstone/products/${productName}"
-if [ "$superDoit_gemstone_version" != "$superDoit_solo_extent_version" ] ; then
-	echo "Downloading solo product tree for $superDoit_solo_extent_version ..."
-	${superDoit_root_dir}/dev/downloadGemStone.sh $superDoit_solo_extent_version
-	productName=GemStone64Bit${superDoit_solo_extent_version}-${productExtension}
+if [ "$superDoit_gemstone_version" != "$superDoit_solo_version" ] ; then
+	echo "Downloading solo product tree for $superDoit_solo_version ..."
+	${superDoit_root_dir}/dev/downloadGemStone.sh $superDoit_solo_version
+	productName=GemStone64Bit${superDoit_solo_version}-${productExtension}
 fi
 
-echo "create solo product symbolic link for $superDoit_solo_extent_version ..."
+echo "create solo product symbolic link for $superDoit_solo_version ..."
 cd ../solo
 ln -s ../products/$productName product
 echo "::set-output name=solo-product-name::$productName"
