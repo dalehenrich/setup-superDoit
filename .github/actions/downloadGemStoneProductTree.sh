5b16b3b0d44a30acc9f303213b163597627c8c44
Dale Henrichs <dale.henrichs@gemtalksystems.com>
2023-09-27 16:33:00 -0700

    restore the logic for creating solo extents

--- .github/actions/downloadGemStoneProductTree.sh
+++ .github/actions/downloadGemStoneProductTree.sh
@@ -20,5 +20,11 @@ echo "gemstone-product-path=${superDoit_root_dir}/gemstone/products/${productNam
 echo "create solo product symbolic link for $superDoit_solo_version ..."
 cd ../solo
 ln -s ../products/$productName product
+echo "Copying extent0.rowan.dbf and extent0.dbf"
+cp ../products/$productName/bin/extent0.rowan.dbf extent0.solo.dbf
+chmod -w extent0.solo.dbf
+cp ../products/$productName/bin/extent0.dbf extent0.dbf
+chmod -w extent0.dbf
+
 echo "solo-product-name=$productName" >> $GITHUB_OUTPUT
 
