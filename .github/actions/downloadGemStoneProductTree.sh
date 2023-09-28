1b029c36abad56a6133462667ba178755d57615d
Dale Henrichs <dale.henrichs@gemtalksystems.com>
2023-09-28 10:24:20 -0700

    using superDoit/bin/install.sh, since it properly handles mac downloads ... the root cause of recent troubles

--- .github/actions/downloadGemStoneProductTree.sh
+++ .github/actions/downloadGemStoneProductTree.sh
@@ -11,20 +11,9 @@ fi
 
 cd ${superDoit_root_dir}/gemstone/products
 echo "Downloading product tree for $superDoit_gemstone_version ..."
-${superDoit_root_dir}/dev/downloadGemStone.sh $superDoit_gemstone_version
+${superDoit_root_dir}/bin/install.sh $superDoit_gemstone_version
 
 productName=GemStone64Bit${superDoit_gemstone_version}-${productExtension}
 echo "gemstone-product-name=$productName" >> $GITHUB_OUTPUT
 echo "gemstone-product-path=${superDoit_root_dir}/gemstone/products/${productName}" >> $GITHUB_OUTPUT
-
-echo "create solo product symbolic link for $superDoit_solo_version ..."
-cd ../solo
-ln -s ../products/$productName product
-echo "Copying extent0.rowan.dbf and extent0.dbf"
-cp ../products/$productName/bin/extent0.rowan.dbf extent0.solo.dbf
-chmod -w extent0.solo.dbf
-cp ../products/$productName/bin/extent0.dbf extent0.dbf
-chmod -w extent0.dbf
-
-echo "solo-product-name=$productName" >> $GITHUB_OUTPUT
 
