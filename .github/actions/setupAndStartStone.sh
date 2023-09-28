6107e130f0ddb6acb2ad3e0ec5f8b44c27e58e53
Dale Henrichs <dale.henrichs@gemtalksystems.com>
2023-09-28 11:17:08 -0700

    remove debugging statements; update lineup

--- .github/actions/setupAndStartStone.sh
+++ .github/actions/setupAndStartStone.sh
@@ -8,19 +8,10 @@ sudo chmod oug+rwx /opt/gemstone
 sudo mkdir /opt/gemstone/locks
 sudo chmod oug+rwx /opt/gemstone/locks
 
-echo "PATH---"
 echo $PATH
-echo "PATH---"
 . $SUPERDOIT_ROOT/dev/defPath.env
-echo "PATH---"
 echo $PATH
-echo "PATH---"
 
-echo "GEMSTONE=$GEMSTONE"
-ls -l $GEMSTONE/bin
-echo "=============================================="
-$GEMSTONE/bin/topaz -h
-echo "=============================================="
 createSuperDoitStone.solo -D --GEMSTONE=$GEMSTONE --stoneDirectory=$GEMSTONE_STONE_DIR --stoneName=$GEMSTONE_STONE_NAME
 
 $GEMSTONE/bin/startstone -l $GEMSTONE_STONE_DIR/logs/${GEMSTONE_STONE_NAME}.log -z $GEMSTONE_SYSTEM_CONF $GEMSTONE_STONE_NAME
