6a4998ede6ef4de6dc3bfda90a1aecddb16d5d97
Dale Henrichs <dale.henrichs@gemtalksystems.com>
2023-09-28 11:08:32 -0700

    get the productName in downloadGemStoneProductTree.sh sorted

--- .github/actions/downloadGemStoneProductTree.sh
+++ .github/actions/downloadGemStoneProductTree.sh
@@ -3,15 +3,25 @@
 set -e
 #set -x
 
-if [ "$PLATFORM" = "macos-10.15" ] ; then
-	productExtension="i386.Darwin"
-else
-	productExtension="x86_64.Linux"
-fi
-
 cd ${superDoit_root_dir}/gemstone/products
 echo "Downloading product tree for $superDoit_gemstone_version ..."
 ${superDoit_root_dir}/bin/install.sh $superDoit_gemstone_version
+
+case "$PLATFORM" in
+   Darwin-arm64)
+		productName="GemStone64Bit${gemstoneversion}-arm64.Darwin"
+		;;
+   Darwin-x86_64)
+		productName="GemStone64Bit${gemstoneversion}-i386.Darwin"
+		;;
+	Linux-x86_64)
+		productName="GemStone64Bit${gemstoneversion}-x86_64.Linux"
+     ;;
+	*)
+		echo "This script should only be run on Mac (Darwin-i386 or Darwin-arm64), or Linux (Linux-x86_64) ). The result from \"uname -sm\" is \"`uname -sm`\""
+		exit 1
+     ;;
+esac
 
 productName=GemStone64Bit${superDoit_gemstone_version}-${productExtension}
 echo "gemstone-product-name=$productName" >> $GITHUB_OUTPUT
