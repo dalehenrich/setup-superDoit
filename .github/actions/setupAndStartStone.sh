a5e0ba5979a62cbb6b0545ea24fcb98d3f1e1f34
Dale Henrichs <dale.henrichs@gemtalksystems.com>
2023-09-27 17:33:28 -0700

    more debugging

--- .github/actions/setupAndStartStone.sh
+++ .github/actions/setupAndStartStone.sh
@@ -9,6 +9,11 @@ sudo mkdir /opt/gemstone/locks
 sudo chmod oug+rwx /opt/gemstone/locks
 
 . $SUPERDOIT_ROOT/dev/defPath.env
+
+echo "GEMSTONE=$GEMSTONE"
+ls -l $GEMSTONE/bin
+$GEMSTONE/bin/topaz -h
+topaz -h
 createSuperDoitStone.solo -D --GEMSTONE=$GEMSTONE --stoneDirectory=$GEMSTONE_STONE_DIR --stoneName=$GEMSTONE_STONE_NAME
 
 $GEMSTONE/bin/startstone -l $GEMSTONE_STONE_DIR/logs/${GEMSTONE_STONE_NAME}.log -z $GEMSTONE_SYSTEM_CONF $GEMSTONE_STONE_NAME
